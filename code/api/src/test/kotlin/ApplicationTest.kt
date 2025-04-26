d235c7455c00a2be4a8631e1792271915034a8cf
Nicholas Rutherford <nicholasrutherford@Nicholass-MBP.lan>
2025-04-26 04:11:22 -0500

    based project

---
+++ code/api/src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package com.nciholas.rutherford.habit.vibes.quote
+
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            module()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+        }
+    }
+
+}

aded182db42521acd6e60e04d266e5e7e388e00e
Nicholas Rutherford <nicholasrutherford@Nicholass-MacBook-Pro.local>
2025-05-24 12:22:40 -0500

    comment out ktlint for right now

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.0"
     id("application")
     alias(libs.plugins.ktor)
-    alias(libs.plugins.ktlint)
+    //alias(libs.plugins.ktlint)
     alias(libs.plugins.kotlin.plugin.serialization)
 }
 
